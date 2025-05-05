841c9ef4c0fd3a9bb4486806aacd2119900328dc
BELEFQIH Mohammed <mblq02@gmail.com>
2025-05-05 21:03:33 +0100

    Update liste_stagiaires.php

--- views/liste_stagiaires.php
+++ views/liste_stagiaires.php
@@ -20,19 +20,21 @@ ob_start();
 
         </thead>
         <tbody>
-        <?php foreach ($stagiaires as $stagiaire): ?>
-            <tr>
-                <td><?= $stagiaire->id ?></td>
-                <td><?= $stagiaire->nom ?></td>
-                <td><?= $stagiaire->prenom ?></td>
-                <td><?= $stagiaire->age ?></td>
-                <td><?= $stagiaire->login ?></td>
-                <td>
-                    <a href="index.php?action=edit&id=<?php echo $stagiaire->id?>" class="btn btn-success btn-sm">Modifier</a>
-                    <a href="index.php?action=delete&id=<?php echo $stagiaire->id?>" class="btn btn-danger btn-sm">Supprimer</a>
-                </td>
-            </tr>
-        <?php endforeach; ?>
+        <?php if (isset($stagiaires)): ?>
+            <?php foreach ($stagiaires as $stagiaire): ?>
+                <tr>
+                    <td><?= $stagiaire->id ?></td>
+                    <td><?= $stagiaire->nom ?></td>
+                    <td><?= $stagiaire->prenom ?></td>
+                    <td><?= $stagiaire->age ?></td>
+                    <td><?= $stagiaire->login ?></td>
+                    <td>
+                        <a href="index.php?action=edit&id=<?php echo $stagiaire->id?>" class="btn btn-success btn-sm">Modifier</a>
+                        <a href="index.php?action=delete&id=<?php echo $stagiaire->id?>" class="btn btn-danger btn-sm">Supprimer</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
         </tbody>
     </table>
 <?php $content = ob_get_clean(); ?>
